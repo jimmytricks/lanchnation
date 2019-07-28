eb465fd6a6b6bd17bb6f23f529688a18b1059734
Hixie4 <jhixie4@gmail.com>
2019-07-27 21:01:23 -0700

    functioning with current and past years

--- src/components/sheetRequest.vue
+++ src/components/sheetRequest.vue
@@ -32,6 +32,43 @@
         </tr>
       </tbody>
     </table>
+
+    <h2>In play bets</h2>
+
+    <h2>Previous Years</h2>
+
+    <table class="overall">
+      <thead>
+        <tr>
+          <th colspan="2" v-if="year2018">{{ year2018.yearheading[0] }}</th>
+        </tr>
+        <tr>
+          <th v-for="heading in year2018.tableheading" v-bind:key="heading.id">{{ heading }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="persons in year2018.names" v-bind:key="persons.id">
+          <td v-for="entry in persons" v-bind:key="entry.id">{{ entry }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <table class="overall">
+      <thead>
+        <tr>
+          <th colspan="2" v-if="currentYear">2018/2019 Awards</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="details in year2018.details" v-bind:key="details.id">
+          <td
+            v-for="entrydetails in details"
+            v-bind:key="entrydetails.id"
+            class="season-details"
+          >{{ entrydetails }}</td>
+        </tr>
+      </tbody>
+    </table>
   </main>
 </template>
 
@@ -47,8 +84,10 @@ export default {
       endpoint: "https://sheets.googleapis.com/v4/spreadsheets/",
       spreadsheet: process.env.VUE_APP_SPREADSHEETID,
       apiKey: process.env.VUE_APP_GOOGLE,
-      currentYearSelection: "/values/currentapisheet!A1:B11?",
+      currentYearSelection: "/values/currentapisheet!A1:B10?",
+      yearSelection2018: "/values/2018apisheet!A1:B10?",
       currentSheetData: "",
+      sheetData2018: "",
       currentYearData: "",
       previousYearsData: "",
       numberOfPeople: 4,
@@ -56,26 +95,38 @@ export default {
       numberOfHeadings: 2,
       numberOfHeadingsAndPeople: 6,
       currentYear: "",
+      year2018: "",
       yearCollections: []
     };
   },
   created() {
-    this.init();
+    this.initCurrentYear();
+    this.init2018();
   },
   mounted() {},
   methods: {
     // Fetch data and assign it to sheetdata var
-    async fetchData(yearSelect, vuePropToSaveItTo) {
-      console.log(vuePropToSaveItTo);
+    async fetchData(yearSelect, specificYear) {
       let response = await fetch(
         `${this.endpoint}${this.spreadsheet}${yearSelect}$key=${this.apiKey}`
       );
       let data = await response.json();
-      this.currentSheetData = data;
+
+      if (specificYear == "currentSheetData") {
+        this.currentSheetData = data;
+      } else {
+        this.sheetData2018 = data;
+      }
     },
-    createTable() {
-      let tableData = this.currentSheetData.values;
-      const tableObj = {};
+    createTable(specificYear) {
+      let tableData;
+      if (specificYear == "currentSheetData") {
+        tableData = this.currentSheetData.values;
+      } else {
+        tableData = this.sheetData2018.values;
+      }
+
+      let tableObj = {};
       // Constructor to create year collections within container object
 
       const yearCollection = tableData => {
@@ -100,65 +151,24 @@ export default {
           e < tableData.length - this.numberOfHeadingsAndPeople;
           e++
         ) {
-          console.log("test");
           tableObj.details[e] = tableData[e + this.numberOfHeadingsAndPeople];
         }
-        console.log(tableObj.details);
-        this.currentYear = tableObj;
+
+        if (specificYear == "currentSheetData") {
+          this.currentYear = tableObj;
+        } else {
+          this.year2018 = tableObj;
+        }
       };
 
       yearCollection(tableData);
     },
-    // spliceJSONByYear() {
-    //   let dataToBeSorted = this.sheetData.values;
-    //   let yearCollectionContainer = [];
-
-    //   // Counter for creating js object and adding it to
-    //   let counter = 0;
-
-    //   // Plus two, as it adds year heading and table row heading
-    //   let totalMaxRows = (this.numberOfPeople + 2) * this.numberOfYears;
-
-    //   // The amount of rows for each year collection
-    //   let rowsPerCollection = totalMaxRows / this.numberOfYears;
-
-    //   // Recursively slice arrays and create new year collection obj with each set
-    //   function recursivelySliceArray(startingArray) {
-    //     if (startingArray == totalMaxRows) {
-    //       counter = 0;
-    //       return;
-    //     } else {
-    //       let startingSlicePoint = startingArray;
-    //       let endingSlicePoint = startingSlicePoint + rowsPerCollection;
-    //       let slicedYearCollection = dataToBeSorted.slice(
-    //         startingSlicePoint,
-    //         endingSlicePoint
-    //       );
-    //       yearCollectionContainer[counter] = new yearCollection(
-    //         slicedYearCollection
-    //       );
-    //       counter++;
-    //       return recursivelySliceArray(endingSlicePoint);
-    //     }
-    //   }
-    //   // Call the array with max total rows
-    //   recursivelySliceArray(0);
-    //   this.yearCollections = yearCollectionContainer;
-
-    //   // Constructor to create year collections within container object
-    //   function yearCollection(slicedJSONObject) {
-    //     this.yearheading = slicedJSONObject[0];
-    //     this.tableheading = slicedJSONObject[1];
-    //     this.names = [];
-    //     // Loop the names
-    //     // In here sort the names by total win, also add in a prop for first, second third and 4th
-    //     for (let e = 2; e < rowsPerCollection; e++) {
-    //       this.names[e - 2] = slicedJSONObject[e];
-    //     }
-    //   }
-    // },
-    sortedYearCollectionsByWinAmount() {
-      this.currentYear.names.sort(compare);
+    sortedYearCollectionsByWinAmount(specificYear) {
+      if (specificYear == "currentSheetData") {
+        this.currentYear.names.sort(compare);
+      } else {
+        this.year2018.names.sort(compare);
+      }
 
       function compare(a, b) {
         if (parseFloat(a[1]) > parseFloat(b[1])) {
@@ -170,15 +180,15 @@ export default {
         return 0;
       }
     },
-    test() {
-      // console.log(this.yearCollections);
-      console.log(this.headingsAndPeople);
+    async initCurrentYear() {
+      await this.fetchData(this.currentYearSelection, "currentSheetData");
+      this.createTable("currentSheetData");
+      this.sortedYearCollectionsByWinAmount("currentSheetData");
     },
-    async init() {
-      await this.fetchData(this.currentYearSelection, 'currentSheetData');
-      this.createTable();
-      this.sortedYearCollectionsByWinAmount();
-      this.test();
+    async init2018() {
+      await this.fetchData(this.yearSelection2018, "sheetData2018");
+      this.createTable("sheetData2018");
+      this.sortedYearCollectionsByWinAmount("sheetData2018");
     }
   }
 };
